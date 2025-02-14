55f8761663bb999c7347152f1990041ae071b92f
sjhalani7 <86908164+sjhalani7@users.noreply.github.com>
2025-02-13 21:42:28 -0800

    model init file

---
+++ open-ai/model.py
@@ -0,0 +1,60 @@
+from openai import OpenAI
+import config
+
+client = OpenAI(api_key=config.open_ai_api_key)
+
+
+def query_model(prompt_file, question_query):
+    with open(prompt_file, 'r') as file:  
+        developer_query = file.read()
+    
+    completion = client.chat.completions.create(
+    model = "gpt-4o",
+    messages = [
+        {"role": "developer", "content": f"{developer_query}"},
+        {
+            "role": "user",
+            "content": f"{question_query}"
+        }
+    ],
+    response_format = {
+        "type": "json_schema",
+        "json_schema":{ 
+            "name": "qa_schema",
+            "schema": {
+                "type": "object",
+                "properties": {
+                "question_id": {
+                    "type": "int",
+                    "description": "Interal question_id of question"
+                },
+                "question": {
+                    "type": "string",
+                    "description": "The question being asked."
+                },
+                "answer": {
+                    "type": "string",
+                    "description": "The answer to the question."
+                },
+                "explanation": {
+                    "type": "string",
+                    "description": "A detailed explanation supporting the answer."
+                }
+                },
+                "required": [
+                "question",
+                "answer",
+                "explanation"
+                ],
+                "additionalProperties": False
+            },
+            "strict": True
+        }
+    }
+    )
+    print(completion.choices[0])
+
+        
+    
+
+query_model('open-ai/dev_prompt.txt')
