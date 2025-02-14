e3b4ee6828bc317e8dd23c9cf26864c6f6e31006
sjhalani7 <86908164+sjhalani7@users.noreply.github.com>
2025-02-14 00:45:06 -0800

    alter table answers instead of questions

--- open-ai/import_explanations_to_db.py
+++ open-ai/import_explanations_to_db.py
@@ -6,29 +6,29 @@ import re
 
 def get_questions_for_explanations():
     query_constructor = create_query(
-        select=['questions.question_id', 'questions.question_text', ' answers.answer_text'],
+        select=['answers.answer_id', 'questions.question_id', 'questions.question_text', ' answers.answer_text'],
         from_table=['questions'],
         join=[('answers', "answers.question_id = questions.question_id", "INNER JOIN")],
         where=[("answers.answer_type", "right")]
     )
-    query_constructor.where_raw("questions.answer_explanations IS NULL")
+    query_constructor.where_raw("answers.answer_explanation IS NULL")
     questions_list = get_query_results(query_constructor)
     return questions_list
 
 
 def get_explanations_update_db(questions_list):
-    for question_id, question_text, answer_text in questions_list:
+    for answer_id, question_id, question_text, answer_text in questions_list:
         inp_str = {
             'question': question_text,
             'answer': answer_text
         }
 
-        print(question_id, question_text, answer_text, "\n\n\n")
+        print(answer_id, question_id, question_text, answer_text, "\n\n\n")
         model_explanation = model.query_model(inp_str)
         model_explanation_json = json.loads(model_explanation)
         answer_explanation = model_explanation_json["explanation"]
         answer_explanation = re.sub(r'"', "'", answer_explanation)
-        query_constructor = update_table_query('questions', ("answer_explanations", answer_explanation), ("question_id", question_id))
+        query_constructor = update_table_query('answers', ("answer_explanation", answer_explanation), ("answer_id", answer_id))
         execute_update_query(query_constructor)
 
         
