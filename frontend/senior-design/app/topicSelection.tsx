41dba8f10de6aa389a7ba1407330b5df87a1c0e2
Brian Wiebe <51461287+wiebecoding@users.noreply.github.com>
2025-05-15 18:51:09 -0700

    Fixed object return on free response incorrect.

--- frontend/senior-design/app/topicSelection.tsx
+++ frontend/senior-design/app/topicSelection.tsx
@@ -91,6 +91,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         width: '100%',
+        paddingTop: 50,
         paddingHorizontal: 20,
     },
     title: {
